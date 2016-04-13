602c4c30ae3560dd89314df931873a64264cfdde
Cal Stephens <calstephens98@gmail.com>
2016-04-13 17:11:14 -0400

    UI prototype

---
+++ Space Agency/Items.swift
@@ -0,0 +1,40 @@
+//
+//  Items.swift
+//  Space Agency
+//
+//  Created by Cal on 4/13/16.
+//  Copyright © 2016 Cal Stephens. All rights reserved.
+//
+
+let SAItems = [
+    
+    Item(name: "Fireworks", income: 10, price: 100, desc: "You figure you have to start your space program somewhere, so you go out to Joe's Wacky Rocket Shack and pick up a few 4th of July fireworks on clearence. This will be some valuable science."),
+    
+    Item(name: "Bottle Rocket", income: 100, price: 800, desc: "After a few close shaves and burnt eyebrows, you finally figure out this whole \"rocket\" thing. TODO: Maybe pick up some Mentos."),
+    
+    Item(name: "Weather Balloon", income: 500, price: 10000, desc: "With a bottle rocket altitude record of an astounding 146 meters, you realize that a balloon could get a little bit higher."),
+    
+    Item(name: "Missile", income: 1500, price: 35000, desc: "Your military thinks you should be doing something a little more productive. They enlist you to create missles to \"protect democracy\"."),
+    
+    Item(name: "ICBM", income: 5000, price: 100000, desc: "Those missles were impressive, but the only way to make them go farther is to make them even bigger.")
+]
+
+
+class Item {
+    
+    let name: String
+    let income: Int
+    let price: Int
+    let description: String
+    
+    var unlocked: Bool = false
+    let count = 0
+    
+    init(name: String, income: Int, price: Int, desc: String) {
+        self.name = name
+        self.income = income
+        self.price = price
+        self.description = desc
+    }
+    
+}
--- Space Agency/ViewController.swift
+++ Space Agency/ViewController.swift
@@ -8,18 +8,117 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//MARK: Game Variables
+var SABalance = 0
+var SAIncome = 10
 
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    var incomeTimer: NSTimer!
+    @IBOutlet weak var balanceLabel: UILabel!
+    @IBOutlet weak var incomeLabel: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //schedule the income timer
+        self.incomeTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(self.incomeTick(_:)), userInfo: nil, repeats: true)
+        
+        NSRunLoop.currentRunLoop().addTimer(self.incomeTimer, forMode: NSRunLoopCommonModes)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func incomeTick(timer: NSTimer) {
+        SABalance += SAIncome
+        updateLabels()
+    }
+    
+    //MARK: - Animations
+    
+    func updateLabels() {
+        let balanceString = SABalance.formatedAsMoney()
+        let incomeString = "\(SAIncome.formatedAsMoney()) per day"
+        
+        func animateChange(label: UILabel, to: String) {
+            label.text = to
+            
+            UIView.animateWithDuration(0.15, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.0, options: [], animations: {
+                    label.transform = CGAffineTransformMakeScale(1.2, 1.2)
+            }, completion: nil)
+            
+            UIView.animateWithDuration(0.5, delay: 0.2, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.0, options: [], animations: {
+                    label.transform = CGAffineTransformIdentity
+            }, completion: nil)
+        }
+        
+        if balanceLabel.text != balanceString {
+            animateChange(balanceLabel, to: balanceString)
+        }
+        
+        if incomeLabel.text != incomeString {
+            animateChange(incomeLabel, to: incomeString)
+        }
     }
 
+    //MARK: - Collection View delegate
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return SAItems.count;
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("item", forIndexPath: indexPath) as! ItemCell
+        cell.decorateForItem(SAItems[indexPath.item])
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        let width = (UIScreen.mainScreen().bounds.width / 2) - 10
+        return CGSizeMake(width, width)
+    }
+    
+}
 
+class ItemCell : UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    func decorateForItem(item: Item) {
+        
+        nameLabel.text = item.name
+        //image.image = UIImage(named: item.name)
+        subtitleLabel.text = "\(item.price.formatedAsMoney()) for \(item.income.formatedAsMoney())/day"
+        countLabel.text = "x \(item.count)"
+    }
+    
+}
+
+extension Int {
+    
+    func formatedAsMoney() -> String {
+        let numberString = "\(self)" as NSString
+        var moneyString = ""
+        
+        var index = 0
+        while index != numberString.length {
+            
+            //prepend character
+            let range = NSMakeRange(numberString.length - index - 1, 1)
+            let char = numberString.substringWithRange(range)
+            moneyString = "\(char)\(moneyString)"
+            
+            index += 1;
+            //prepend comma if necessary
+            if index % 3 == 0 && index != numberString.length {
+                moneyString = ",\(moneyString)"
+            }
+            
+        }
+        
+        return "$\(moneyString)"
+    }
 }
 
